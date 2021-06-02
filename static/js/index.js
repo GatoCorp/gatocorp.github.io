6354d539a14768664ca276abb2a229732bab920a
DR-MATTH <mmarinv@est.emi.edu.bo>
2021-06-01 22:49:29 -0400

    _

--- static/js/index.js
+++ static/js/index.js
@@ -11,7 +11,7 @@ function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">EMI Santa Cruz</a>
+        <Link class="navbar-brand" to="/">EMI Santa Cruz</Link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03"
           aria-controls="navbarColor03" aria-expanded="true" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
