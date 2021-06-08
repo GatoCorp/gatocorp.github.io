2bd1e53e9bae841833ab3b56ed81201004fb4a70
GusSanchezC <gasanchezc@est.emi.edu.bo>
2021-06-07 22:50:46 -0400

    asd

--- static/JS/registro.js
+++ static/JS/registro.js
@@ -59,7 +59,7 @@ async function agregarEstudiante() {
         ci: parseInt(ci),
         correo: correo,
         carrera: carrera,
-        semestre: parseInt(1, 10), // quitar esta hardcodeada
+        semestre: 1, // quitar esta hardcodeada
         foto: foto.files[0],
         certifNac: certifNac.files[0],
         titulo: titulo.files[0],
@@ -97,9 +97,6 @@ fetch(`${API}/carreras`)
     .then(data => llenarSelect(data))
     .catch(err => console.log(err))
 
-const btnenviar = document.getElementById('btnenviar')
-btnenviar.addEventListener('click', agregarEstudiante)
-
 const defaultBtn = document.getElementById("imagen")
 defaultBtn.addEventListener("change", agregarImagen)
 
@@ -136,5 +133,71 @@ async function handleArchivo(e) {
 
 }
 
-const btnArchivo = document.getElementById('file')
-btnArchivo.addEventListener('change', handleArchivo)
+//const btnArchivo = document.getElementById('file')
+//btnArchivo.addEventListener('change', handleArchivo)
+
+//Validacion del formulario
+const formulario = document.getElementById('formulario')
+const inputs = document.querySelectorAll('#formulario input')
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+const campos = {
+    nombre: false,
+    apellido: false,
+    ci: false,
+    correo: false
+}
+
+const validarFormulario = (e) => {
+    switch(e.target.id) {
+        case "nombre":
+            validarCampo(expresiones.nombre,e.target,'nombre')
+        break
+        case "apellido":
+            validarCampo(expresiones.nombre,e.target,'apellido')
+        break
+        case "ci":
+            validarCampo(expresiones.telefono,e.target,'ci')
+        break
+        case "correo":
+            validarCampo(expresiones.correo,e.target,'correo')
+        break
+    }
+}
+const validarCampo = (expresion,input,campo) => {
+    if (expresion.test(input.value)){
+        document.getElementById(`div__${campo}`).classList.remove('has-danger')
+        document.getElementById(input.id).classList.remove('is-invalid')
+        document.getElementById(`div__${campo}`).classList.add('has-success')
+        document.getElementById(input.id).classList.add('is-valid')
+        campos[campo] = true
+    } else {
+        document.getElementById(`div__${campo}`).classList.add('has-danger')
+        document.getElementById(input.id).classList.add('is-invalid')
+        document.getElementById(`div__${campo}`).classList.remove('has-success')
+        document.getElementById(input.id).classList.remove('is-valid')
+        campos[campo] = false
+    }
+}
+inputs.forEach((input)=>{
+    input.addEventListener('keyup', validarFormulario)
+    input.addEventListener('blur', validarFormulario)
+})
+
+formulario.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    if(campos.nombre && campos.apellido && campos.ci && campos.correo){
+        await agregarEstudiante()
+        formulario.reset() 
+    }else {
+        document.getElementById('alerta').classList.add('alert-activo')
+    }
+})
