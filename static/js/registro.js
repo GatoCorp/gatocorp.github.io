33fc39b5d7d9be39db31f3615a2f76154c6e1a70
DR-MATTH <mmarinv@est.emi.edu.bo>
2021-06-10 00:05:41 -0400

    _

--- static/js/registro.js
+++ static/js/registro.js
@@ -189,8 +189,6 @@ function mostrarAlertError() {
 }
 //Cerrar mensaje de error
 document.getElementById('alert-button').addEventListener('click', () => {
-    console.log('click')
-    console.log(document.getElementById('alerta').classList)
     document.getElementById('alerta').classList.remove('alert-activo')
 })
 
