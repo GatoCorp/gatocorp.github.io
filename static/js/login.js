80ff77cb27e4cfa8d1477a92bede80198e9d5449
GusSanchezC <gasanchezc@est.emi.edu.bo>
2021-06-14 11:37:24 -0400

    asd

--- static/js/login.js
+++ static/js/login.js
@@ -1,5 +1,5 @@
-//const API = 'https://gatocorpapi.herokuapp.com'
-const API = "http://localhost:8000" //comentar despues de usar
+const API = 'https://gatocorpapi.herokuapp.com'
+//const API = "http://localhost:8000" //comentar despues de usar
 
 const ingresar = document.getElementById('login')
 
@@ -8,19 +8,28 @@ async function verificar() {
     try {
         const codEst = document.getElementById('codigo').value
         const contra = document.getElementById('contrasena').value
-        let info
-        await fetch(`${API}/login/${codEst}/${contra}`)
+        await fetch(`${API}/login/estudiante`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              usuario: codEst,
+              contrasena: contra
+            })
+          })
             .then(response => response.json())
-            .then(data => info = data)
-            .catch(err => console.log(err))
-        if (info == undefined || info != true ){
-            document.getElementById('alerta').classList.add('alert-activo')
-        }
-        else {
-            document.cookie = ";max-age=0;"
-            document.cookie = `${codEst}; path=student.html;`
-            window.location.href = 'student.html'
-        }
+            .then(data => {
+              if (data.correcto) {
+                document.cookie = ";max-age=0;"
+                document.cookie = `${codEst}; path=student.html;`
+                window.location.href = 'student.html'
+              }
+              else {
+                document.getElementById('alerta').classList.add('alert-activo')
+              }
+            })
+            .catch(error => console.log(error))
     } catch {
         document.getElementById('alerta').classList.add('alert-activo')
     }
