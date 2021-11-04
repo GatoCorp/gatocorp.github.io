46a5661cb477076b42e21e10c5517c98b8b4e4ba
DR-MATTH <mmarinv@est.emi.edu.bo>
2021-11-04 19:48:31 -0400

    _

--- static/js/perfil.js
+++ static/js/perfil.js
@@ -1,19 +1,18 @@
 const API = 'https://gatocorpapi.herokuapp.com'
 
-const codigo = document.cookie.match(/codigo=([A-Z]\d{4}-\d{1});/)[1]
+const codigo = document.cookie.match(/codigo=([A-Z]\d{4}-\d{1})/)[1]
 console.log('codigo de la cookie:', codigo)
 document.addEventListener('DOMContentLoaded', function () {
-    fetch(`${API}/estudiantes/${codigo}`)[0]
-        .then(response => response.json())
-        .then(data => {
-            console.log('data de la response:', data)
-            document.getElementById('codigo').value = data[0].codigo
-            document.getElementById('nombre').value = `${data[0].nombre} ${data[0].apellido}`
-            document.getElementById('carrera').value = data[0].carrera
-            document.getElementById('semestre').value = data[0].semestre
-            document.getElementById('correo').value = data[0].correo
-            document.getElementById('foto').src = data[0].foto
+    fetch(`${API}/estudiantes/codigo`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          codigo: codigo
         })
+      }).then(response => response.json())
+        .then(data => console.log(data))
         .catch(err => console.log(err))
 })
 
